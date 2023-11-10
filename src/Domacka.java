384427155002141beeed146544e63ebc0c927555
Ksenia Poshekhova <kposehova@gmail.com>
2023-11-10 19:25:40 +0300

    Unit commit

--- src/Domacka.java
+++ src/Domacka.java
@@ -1,11 +1,11 @@
 public class Domacka {
 
     public static void main(String[] args) {
-        int deposit  = 13_676;
+        int deposit = 13_676;
         int forEveryDeposited = 20;
 
 
         System.out.println("Количество бонусов составляет");
-        System.out.println(deposit / forEveryDeposited );
+        System.out.println(deposit / forEveryDeposited);
     }
 }
--- src/Plan.java
+++ src/Plan.java
@@ -3,17 +3,12 @@ public class Plan {
     public static void main(String[] args) {
         System.out.println();
         int amount = 100;
-        int depositMoney = 300;
-        // int mile = 1;
-
-        System.out.println("Общая сумма на счету клиента с учётом бонусных рублей составляет :");
-        System.out.println(amount + depositMoney);
-        int balanceOfFunds = 100;
         int replenishmentAmount = 1100;
-
         // int percent = 11;
         int bonys = 11;
+
+
         System.out.println("Общая сумма на счету клиента с учётом бонусных рублей составляет :");
-        System.out.println(balanceOfFunds + replenishmentAmount + bonys);
+        System.out.println(amount + replenishmentAmount + bonys);
     }
 }
